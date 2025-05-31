846737025fb91456aaf53333c71048825f43730c
Manos <138156574+M4n0zz@users.noreply.github.com>
2025-05-31 14:15:27 +0300

    Update ItemGiver+ Standalone RB.asm
    Added delete functionality

--- ItemGiver+/Asm/ItemGiver+ Standalone RB.asm
+++ ItemGiver+/Asm/ItemGiver+ Standalone RB.asm
@@ -8,63 +8,87 @@ https://github.com/M4n0zz/QuickRGBDS
 
 include "pokered.inc"
 
+DEF nicknameaddress = $d8b5
+
 SECTION "ItemGiver+", ROM0
 
-
-LOAD "NicknameWriterPayload", WRAMX[$D8B5]
+LOAD "NicknameWriterPayload", WRAMX[nicknameaddress]
 
 start:
-ld 	hl, list					; list header is stored
-ld 	a, l
-ld 	[wListPointer], a
-ld 	a, h
-ld 	[wListPointer + 1], a
+ld  hl, wListPointer			; list header is stored
+ld  de, list
+ld  [hl], e
+inc hl
+ld  [hl], d
 
-xor 	a
+xor a
 .loop1
 inc a
-inc hl
-ld 	[hl], a
-cp 	a, $73						; list is stopped before glitch names appear
-jr 	nz, .loop1
+inc de
+ld  [de], a
+cp  a, $73						    ; list is stopped before glitch names appear
+jr  nz, .loop1
 
-ld 	a, $c4						; list continues from HMs 
+ld  a, $c4						    ; list continues from HMs 
 
 .loop2
-inc hl
-ld 	[hl], a
+inc de
+ld  [de], a
 inc a
-and a							    ; up to cancel item
-jr 	nz, .loop2
+and a							        ; up to cancel item
+jr  nz, .loop2
 
-ld 	[wPrintItemPrices], a
-ld 	[wCurrentMenuItem], a
-dec a
-ld 	[wMaxItemQuantity], a 
-ld 	a, $04
-ld 	[wListMenuID], a
+ld  [wCurrentMenuItem], a
+ld  hl, wPrintItemPrices	
+ld  [hl+], a					    ; wListMenuID
+ld  [hl], $04
+dec a							        ; $ff
+ld  l, LOW(wMaxItemQuantity)
+ld  [hl], a
 
 reload:
 call DisplayListMenuID
 and a, a	
-jr 	nz, continue				; end if B is pressed
-xor a
-ld 	[wListScrollOffset], a
+jr  nz, continue				  ; end if B is pressed
+
+ldh a, [hJoyInput]
+xor a, $02						    ; so when B is pressed, a == 00
+jr  z, exit
+
+ld  hl, wNumBagItems			; checks for non zero items
+ld  a, [hl]
+and a
+jr  z, reload
+
+dec a							        ; removes last item in full quantity
+ld  [hli], a
+add a, a
+add a, l
+ld  l, a
+ld  a, $ff
+ld  [hl], a
+call WaitForSoundToFinish
+ld  a, $ab
+jr  playsound
+
+exit:
+ld  [wListScrollOffset], a
 ret
 
 continue:
 call DisplayChooseQuantityMenu
 and a, a	
-jr 	nz, reload					; if B pressed reload menu
-ld 	a, [wItemQuantity]
-ld 	c, a						    ; b, c = id, quantity
-ld 	a, [wCurItem]
-ld 	b, a
+jr  nz, reload					  ; if B pressed reload menu
+ld  hl, wItemQuantity
+ld  c, [hl]						    ; b, c = id, quantity
+ld  l, LOW(wCurItem)
+ld  b, [hl]
 call GiveItem
-ld 	a, $86         			; Load the sound identifier [86 == levelup sound]
-call PlaySound      		; Play the sound
+ld  a, $86         				; Load the sound identifier [86 == levelup sound]
+playsound:
+call PlaySound      			; Play the sound
 
-jr 	reload						  ; reload menu
+jr  reload						    ; reload menu
 
 list:
 db $ae
