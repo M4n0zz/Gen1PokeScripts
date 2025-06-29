c47c8adf9e8367b94eb3326228a0b78a299ffe73
Manos <138156574+M4n0zz@users.noreply.github.com>
2025-06-29 15:14:23 +0300

    Add files via upload

---
+++ DexGiver+/README.md
@@ -0,0 +1,20 @@
+# 🥚 DexGiver+
+
+Get any pokemon in any level — instantly and effortlessly!
+
+# ![preview](DexGiver+.jpg)
+
+DexGiver+ introduces a smooth, user-friendly selection menu based on the Pokédex engine, giving you full control and visibility over your collection. Perfect for game customization, testing, or just having fun.
+
+### ⚠️ Warning
+
+This script uses an OAM DMA hijack to bypass certain ROM limitations.
+Any other active DMA hijack will stop working while this is running.
+
+-----
+### Installation Options
+
+Choose the format that best fits your setup:
+- Installer Version: Permanently installs PokeTeacher+ at a specific memory address within the TimOS environment.
+Perfect for long-term use.
+- Standalone Version: A temporary version that runs until a trainer battle starts. Great for single session or testing.
---
+++ DexGiver+/Yellow/DexGiver+ Y Standalone.asm
@@ -0,0 +1,160 @@
+/*
+
+Source is compiled with QuickRGBDS
+https://github.com/M4n0zz/QuickRGBDS
+
+Do use the latest *.inc files from the repo, to avoid compilation errors
+
+*/
+
+
+include "pokeyellow.inc"
+
+DEF nicknameaddress           = $d8b4
+DEF stackaddress              = $dfd5
+DEF returnaddress             = ShowPokedexMenuexitPokedex-2
+
+
+SECTION "DexGiver+", ROM0
+
+LOAD "NicknameWriterPayload", WRAMX[nicknameaddress]
+
+hijack:
+di                            ; because DMA routine triggers out of sync with our payload
+ld   hl, hDMARoutine          ; we stop all interrupts to avoid crashes
+ld   [hl], $cd                ; $ff80 is replaced with command "call", to jump to our custom address
+inc  hl
+ld   [hl], low(pcall)         ; $ff81 and $ff82 hold hijack's custom address
+inc  hl
+ld   [hl], high(pcall)
+inc  hl
+ld   [hl], $e2                ; ldh [c], a command is put to allow hijack to return dma's original code 
+ei                            ; dma code modification is done, interrupts are enabled again
+
+start:
+; pokedex flags backup
+ld   hl, wPokedexSeen
+ld   de, backup
+ld   bc, $0013
+push de
+push bc
+push hl
+
+push bc
+push hl
+call CopyData
+
+pop  hl
+pop  bc
+dec  bc                       ; we ff 1 byte less
+xor  a
+dec  a                        ; $ff, all bits set
+call FillMemory               ; Set BC bytes of A starting from address HL
+ld   [hl], $7f                ; last byte is set manually so 151 pokemon appear instead of 152
+
+
+xor  a                        ; preset zero to return if nothing is selected
+ld   [wCurPartySpecies], a
+
+ld   b, $10                   ; ROM bank $10
+ld   hl, ShowPokedexMenu      ; route address
+call Bankswitch
+
+; unloads dma hijack
+di                            ; because DMA routine triggers out of sync with our payload
+ld   hl, hDMARoutine          ; we stop all interrupts to avoid crashes
+ld   [hl], $3e                ; $ff80-$ff83 OAM DMA's original values are restored
+inc  hl
+ld   [hl], $c3
+inc  hl
+ld   [hl], $e0
+inc  hl
+ld   [hl], $46
+ei
+
+; restores seen flags
+pop  de
+pop  bc
+pop  hl                       ; backup
+push bc                       ; bytes
+push de                       ; seens start
+call CopyData
+
+; merges seen with own
+pop  hl                       ; hl = own end, de = seen end
+pop  bc
+.loop
+dec  de
+dec  hl
+ld   a, [de]
+or   a, [hl]
+ld   [de], a
+dec  c
+jr   nz, .loop
+
+call LoadScreenTilesFromBuffer2 ; restore saved screen
+call Delay3
+call LoadGBPal
+jp   UpdateSprites
+
+
+pcall:                        ; now dma hijack always points to this address
+call dmapayload               ; it calls our custom dma payload
+
+.endoam                       ; after our custom payload returns, values are set up to continue to dma routine
+ld   c, $46
+ld   a, $c3
+ret	
+
+dmapayload:                   ; we check specific addresses in stack to detect the address
+ld   hl, stackaddress         ; HandleMenuInput is going to return to
+ld   a, low(returnaddress)    ; if detected, we set our custom address (hijackpayload)
+cp   a, [hl]
+ret  nz
+
+inc  hl
+;ld   a, high(returnaddress)  ; it is the same, $4040
+cp   a, [hl]
+ret  nz
+
+ld   a, high(hijackpayload)
+ld   [hld], a
+ld   a, low(hijackpayload)
+ld   [hl], a
+ret
+
+hijackpayload:                ; when  HandlePokedexListMenu finishes execution it jumps to this script instead
+jp	nc, returnaddress+2
+
+continue:
+ld   a, $64
+ld   [wMaxItemQuantity], a 
+call DisplayChooseQuantityMenu
+and  a, a	
+jr   nz, reload               ; if B pressed reload menu
+
+ld   a, [wCurrentMenuItem]
+ld   b, a
+ld   a, [wListScrollOffset]
+add  a, b                     ; calculates selected item position
+inc 	a
+ld   [wPokedexNum], a
+ld 	b, $10
+ld 	hl, PokedexToIndex       ; changes pokedex id to pokemon id
+call Bankswitch
+ld   a, [wPokedexNum]
+ld   b, a
+ld   a, [wItemQuantity]
+ld   c, a                     ; b = id, c = level
+call GivePokemon
+
+reload:
+call ClearScreen
+jp   ShowPokedexMenusetUpGraphics
+
+backup:
+
+.end
+ENDL
+
+
