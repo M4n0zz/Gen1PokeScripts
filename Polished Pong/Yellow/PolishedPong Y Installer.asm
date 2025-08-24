1a252501b4528e2f7e71fcb5d9efc650d6f00b43
Manos <138156574+M4n0zz@users.noreply.github.com>
2025-08-25 01:03:58 +0300

    Updated for latest TimOS version

--- Polished Pong/Yellow/PolishedPong Y Installer.asm
+++ Polished Pong/Yellow/PolishedPong Y Installer.asm
@@ -52,7 +52,7 @@ add  a, [hl]
 ld   [hl], a
 
 ; write pointers to the correct position
-ld   de, $c7bb                ; start counting from script #1
+ld   de, $c7c0                ; start counting from script #1
 .pointerloop
 inc  e
 inc  e
@@ -323,3 +323,4 @@ ENDL
 DEF scriptnumber = (pointers.end - pointers) / 2
 DEF pointerwidth = pointers.end - pointers
 DEF payloadwidth = end - start
+
