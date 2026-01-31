bd7d32583345ab12cf1a7e0d50f3bcbd7b345785
Manos <138156574+M4n0zz@users.noreply.github.com>
2026-01-31 18:30:39 +0200

    Add files via upload

---
+++ SuperSpeed+/Yellow/SuperSpeed+ TimOS Y.asm
@@ -0,0 +1,124 @@
+/*
+
+Source is compiled with QuickRGBDS
+https://github.com/M4n0zz/QuickRGBDS
+
+*/
+
+
+include "pokeyellow.inc"
+
+
+def stackreturnaddress        = OverworldLoopLessDelay   
+def returnaddress             = OverworldLoopLessDelay+3
+def hardcodedbankswitch3f     = $1001
+def timospointers             = $c7c7
+def timosinstalladr           = $c841
+def nickwriteraddress         = $d8b4
+def topstack                  = $df15
+def stackcheckaddress         = $dffd
+
+
+SECTION "OAM_Hijack", ROM0
+
+LOAD "Installer", WRAMX[nickwriteraddress]
+
+installer:               ; it runs once upon installation
+; change scripts number
+ld   hl, $c6e9           ; number of scripts address
+ld   b, [hl]             ; counter for later
+inc  [hl]                ; increase script number by 1
+
+; write pointers to the correct position
+ld  hl, timospointers    ; start counting from script #1
+.pointerloop
+inc  hl
+inc  hl
+dec  b                   ; if counter = 0, current pointer is the one to change
+jr   nz, .pointerloop    ; else loop until b = 0
+
+; Copy script address to the calculated pointer
+ld   de, start          ; origin
+ld   [hl], e
+inc  hl
+ld   [hl], d
+
+; Copy payloads
+ld   hl, installend
+ld   c, finish-payload + endjack-start
+jp   CopyMapConnectionHeaderloop
+
+installend:
+ENDL
+
+
+LOAD "SuperSpeed", WRAM0[timosinstalladr]
+
+start:
+ld   hl, endjack
+ld   de, topstack
+ld   c, finish - payload
+call CopyMapConnectionHeaderloop
+
+hijack:
+di                       ; because DMA routine triggers out of sync with our payload
+ld   hl, hDMARoutine     ; we stop all interrupts to avoid crashes
+ld   [hl], $cd           ; $ff80 is replaced with command "call", to jump to our custom address
+inc  hl
+ld   [hl], low(payload)  ; $ff81 and $ff82 hold hijack's custom address
+inc  hl
+ld   [hl], high(payload)
+inc  hl
+ld   [hl], $e2           ; ldh [c], a command is put to allow hijack to return dma's original code 
+reti                     ; dma code modification is done, interrupts are enabled again
+
+endjack:
+ENDL
+
+
+LOAD "Payload", WRAMX[topstack]
+;;;;;;;;;;;; Executed by OAM DMA hijack ;;;;;;;;;;;;;;;;
+
+payload:                 ; This is called by DMA routine
+
+; stack check and replace
+ld   hl, stackcheckaddress
+ld   a, [hli]
+cp   a, low(stackreturnaddress)
+jr   nz, .endoam
+ld   a, [hl]
+cp   a, high(stackreturnaddress)
+jr   nz, .endoam
+ld   a, high(speedon)
+ld   [hld], a
+ld   [hl], low(speedon)
+
+.endoam                  ; after our custom payload returns, values are set up to continue to dma routine
+ld   c, $46
+ld   a, $c3
+ret	
+
+; script activates when delayFrame ends in overworld loop
+speedon:
+ldh  a, [hJoyHeld]
+bit  0, a                     ; check if button A is pressed
+jp   z, returnaddress
+
+.loop
+ld   b, $02
+ld   a, [wWalkBikeSurfState]
+inc  a
+cp   a, b
+jr   nz, .nobike
+inc  b
+.nobike
+ld   a, [wWalkCounter]
+cp   a, b
+jp   c, returnaddress
+ld   hl, SpawnPikachu_
+call hardcodedbankswitch3f
+call AdvancePlayerSprite
+jr   .loop
+
+finish:
+ENDL
