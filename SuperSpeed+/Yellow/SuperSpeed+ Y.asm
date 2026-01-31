72ac87c9dbfc604e097e6db6cd04dfef45c56bba
Manos <138156574+M4n0zz@users.noreply.github.com>
2026-01-31 14:47:45 +0200

    Stability update

--- SuperSpeed+/Yellow/SuperSpeed+ Y.asm
+++ SuperSpeed+/Yellow/SuperSpeed+ Y.asm
@@ -8,57 +8,67 @@ https://github.com/M4n0zz/QuickRGBDS
 
 include "pokeyellow.inc"
 
-DEF nickwriteraddress         = $d8b4
-DEF stackcheckaddress         = $dffd
-DEF stackreturnaddress        = $0245   
-DEF returnaddress             = $0248
-DEF hardcodedbankswitch3f     = $1001
+
+def stackreturnaddress        = OverworldLoopLessDelay   
+def returnaddress             = OverworldLoopLessDelay+3
+def hardcodedbankswitch3f     = $1001
+def nickwriteraddress         = $d8b4
+def topstack                  = $df15
+def stackcheckaddress         = $dffd
+
 
 SECTION "OAM_Hijack", ROM0
 
-start:
 LOAD "SuperSpeed", WRAMX[nickwriteraddress]
-     
+
+start:
+ld   hl, endjack
+ld   de, topstack
+ld   c, finish - payload
+call CopyMapConnectionHeaderloop
+
 hijack:
 di                       ; because DMA routine triggers out of sync with our payload
 ld   hl, hDMARoutine     ; we stop all interrupts to avoid crashes
 ld   [hl], $cd           ; $ff80 is replaced with command "call", to jump to our custom address
 inc  hl
-ld   [hl], LOW(pcall)    ; $ff81 and $ff82 hold hijack's custom address
+ld   [hl], low(payload)  ; $ff81 and $ff82 hold hijack's custom address
 inc  hl
-ld   [hl], HIGH(pcall)
+ld   [hl], high(payload)
 inc  hl
 ld   [hl], $e2           ; ldh [c], a command is put to allow hijack to return dma's original code 
-ei                       ; dma code modification is done, interrupts are enabled again
+reti                     ; dma code modification is done, interrupts are enabled again
 
-pcall:                   ; now dma hijack always points to this address
-call payload             ; it calls our custom payload at the end of the script
+endjack:
+ENDL
+
+
+LOAD "Payload", WRAMX[topstack]
+;;;;;;;;;;;; Executed by OAM DMA hijack ;;;;;;;;;;;;;;;;
+
+payload:                 ; This is called by DMA routine
+
+; stack check and replace
+ld   hl, stackcheckaddress
+ld   a, [hli]
+cp   a, low(stackreturnaddress)
+jr   nz, .endoam
+ld   a, [hl]
+cp   a, high(stackreturnaddress)
+jr   nz, .endoam
+ld   a, high(speedon)
+ld   [hld], a
+ld   [hl], low(speedon)
 
 .endoam                  ; after our custom payload returns, values are set up to continue to dma routine
 ld   c, $46
 ld   a, $c3
 ret	
 
-;;;;;;;;;;;; Payload ;;;;;;;;;;;; 
-payload:                 ; This is called by DMA routine
-
-; stack check and replace
-ld   hl, stackcheckaddress
-ld   a, [hli]
-cp   a, LOW(stackreturnaddress)
-ret  nz
-ld   a, [hl]
-cp   a, HIGH(stackreturnaddress)
-ret  nz
-ld   a, HIGH(.speedon)
-ld   [hld], a
-ld   [hl], LOW(.speedon)
-ret
-
 ; script activates when delayFrame ends in overworld loop
-.speedon
+speedon:
 ldh  a, [hJoyHeld]
-bit  0, a                ; check if button A is pressed
+bit  0, a                     ; check if button A is pressed
 jp   z, returnaddress
 
 .loop
@@ -77,5 +87,5 @@ call hardcodedbankswitch3f
 call AdvancePlayerSprite
 jr   .loop
 
-.end
+finish:
 ENDL
