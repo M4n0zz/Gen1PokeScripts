ceb64077a034801e4618bfda6e12fe3e3072be2e
Manos <138156574+M4n0zz@users.noreply.github.com>
2026-09-22 22:12:14 +0300

    Add files via upload

---
+++ Effects/[Β] PokeGiver/Readme.md
@@ -0,0 +1,35 @@
+
+## PokeGiver
+
+A minimal script that gives you any non glitch Pokemon to any non glitch level.
+
+# ![preview](pokegiver.png)
+
+**Warning**
+
+The script is temporarily stored in trainer battle data, which is overwritten when a trainer battle begins.
+
+To work around this limitation, use the [MultiScript Installer](https://github.com/M4n0zz/Gen1PokeScripts/tree/main/Effects/%5B%CE%92%5D%20MultiScript) instead.
+
+
+**Red/Blue**
+```
+cd 0f 19 cd 29 24 3e 97 ea 97  
+cf cd 57 2d a7 c0 fa 96 cf 11  
+1e d1 12 06 10 21 f9 4f cd d6  
+35 1a f5 cd 9e 2f 21 09 c4 11  
+6d cd cd 55 19 3e 64 ea 97 cf  
+cd 57 2d c1 a7 20 c7 fa 96 cf  
+4f cd 48 3e 18 be
+```
+
+**Yellow**
+```
+cd dd 16 cd 1c 23 3e 97 ea 96  
+cf cd 51 2c a7 c0 fa 95 cf 11  
+1d d1 12 06 10 21 86 50 cd 84  
+3e 1a f5 cd 93 2e 21 09 c4 11  
+6d cd cd 23 17 3e 64 ea 96 cf  
+cd 51 2c c1 a7 20 c7 fa 95 cf  
+4f cd 59 3e 18 be
+```
---
+++ Effects/[Β] PokeGiver/Red Blue/PokeGiver RB.asm
@@ -0,0 +1,42 @@
+/*
+
+Source is compiled with QuickRGBDS
+https://github.com/M4n0zz/QuickRGBDS
+
+*/
+
+include "pokered.inc"
+
+
+SECTION "PokeGiver", ROM0
+
+start:
+call ClearScreen
+call UpdateSprites            ; removes sprites from screen
+ld   a, 151                   ; total species IDs
+ld   [wMaxItemQuantity],a     ; wMaxItemQuantity write
+call DisplayChooseQuantityMenu
+and  a, a                     ; if a is 0, z flag is set
+ret  nz                       ; if B pressed, then ret
+ld   a, [wItemQuantity]       ; wItemQuantity read
+ld   de, wPokedexNum
+ld   [de], a                  ; pokemon id is stored in wd11e
+ld   b, $10                   ; select bank 16
+ld   hl, PokedexToIndex
+call Bankswitch
+ld   a, [de]
+push af
+call GetMonName
+ld   hl, $c409                ; destination
+ld   de, wNameBuffer          ; origin
+call PlaceString
+ld   a, $64                   ; level 100
+ld   [wMaxItemQuantity], a
+call DisplayChooseQuantityMenu
+pop  bc
+and  a, a                     ; if a is 0, z flag is set
+jr   nz, start	               ; if B pressed go to the beginning
+ld   a, [wItemQuantity]       ; wItemQuantity
+ld   c, a                     ; bc = id, level
+call GivePokemon
+jr   start
